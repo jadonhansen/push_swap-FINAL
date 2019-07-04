01ab49ffcb4c8f7b0d853bb126ed17a358c3aaa8
jadonhansen <jadongavhansen@gmail.com>
2019-07-04 12:55:56 +0200

    update

--- pushaction_one.c
+++ pushaction_one.c
@@ -6,7 +6,7 @@
 /*   By: jhansen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 10:33:01 by jhansen           #+#    #+#             */
-/*   Updated: 2019/07/03 12:59:28 by jhansen          ###   ########.fr       */
+/*   Updated: 2019/07/04 12:55:41 by jhansen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,9 @@ void	ft_sa(t_list *stacka)
 	temp = stacka->num;
 	if (stacka->next != NULL)
 	{
-		stacka = stacka->next;
-		hold = stack->num;
-		stacka->num = temp;
-		stacka = stacka->prev;
+		hold = stacka->next->num;
 		stacka->num = hold;
+		stacka->next->num = temp;
 	}
 }
 
@@ -38,11 +36,9 @@ void	ft_sb(t_list *stackb)
 	temp = stackb->num;
 	if (stackb->next != NULL)
 	{
-		stackb = stackb->next;
-		hold = stackb->num;
-		stackb->num = temp;
-		stackb = stack->prev;
+		hold = stackb->next->num;
 		stackb->num = hold;
+		stackb->next->num = temp;
 	}
 }
 
@@ -50,8 +46,8 @@ void	ft_ss(t_list *stacka, t_list *stackb)
 {
 	if (stacka != NULL && stackb != NULL)
 	{
-		ft_sa(&stacka);
-		ft_sb(&stackb);
+		ft_sa(stacka);
+		ft_sb(stackb);
 	}
 }
 
@@ -60,28 +56,43 @@ void	ft_pa(t_list *stacka, t_list *stackb)
 	t_list	*topb;
 	t_list	*head;
 	
-	head = stacka;
+	if (stacka != NULL)
+		head = stacka;
 	if (stackb->num != NULL)
 	{
 		topb = stackb;
-		if (stackb->next != NULL)
+		if (stackb->next == NULL)
 		{
-
+			free(stackb);
 		}
 		topb->next = *head;
 		topb->prev = NULL;
 		if ((*head) != NULL)
 			(*head)->prev = topb;
 		*head = topb;
+		del_node();	//deleting from stack b
 	}
 }
 
 void	ft_pb(t_list stacka, t_list stackb)
 {
-	int topa;
+	t_list *topa;
+	t_list *head;
 
+	if (stackb != NULL)
+		head = stackb;
 	if (stacka->num != NULL)
 	{
-		topa = stacka->num;
+		topa = stacka;
+		if (stacka->next == NULL)
+		{
+			free(stacka);
+		}
+		topa->next = *head;
+		topa->prev = NULL;
+		if ((*head) != NULL)
+			(*head)->prev = topa;
+		*head = topa;
+		del_node(); //deleting fromm stack a
 	}
 }
