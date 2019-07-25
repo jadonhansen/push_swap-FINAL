894e2e96d097ae88b618696b326c52740a242456
jadonhansen <jadongavhansen@gmail.com>
2019-07-25 17:01:28 +0200

    update

--- push_swap.c
+++ push_swap.c
@@ -6,15 +6,40 @@
 /*   By: jhansen <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/01 15:29:34 by jhansen           #+#    #+#             */
-/*   Updated: 2019/07/18 15:17:13 by jhansen          ###   ########.fr       */
+/*   Updated: 2019/07/25 16:51:24 by jhansen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push_algo(t_stack **stacka, t_stack **stackb)
+int		find_correct_size(t_stack **a)
+{
+	
+}
+
+void	tiny(t_stack **a, t_stack **b)
 {
 
+}
+
+void	small(t_stack **a, t_stack **b)
+{
+
+}
+
+void	push_algo(t_stack **stacka, t_stack **stackb)
+{
+	int	size;
+
+	size = stack_size(stacka);
+	if (size > 1 && size <= 3)
+		tiny(stacka, stackb);
+	else if (size > 3 && size <= 5)
+		small(stacka, stackb);
+	else if (size > 5)
+	{
+		bigboy(stacka, stackb, find_correct_size(stacka));
+	}
 }
 
 int		main(int argc, char **argv)
